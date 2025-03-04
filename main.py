ad11c6664103fe7042ae6f59f886669d9a7377fc
EugenSiva <eugensiva@gmail.com>
2025-03-04 20:39:36 +0100

    Task

---
+++ main.py
@@ -0,0 +1,18 @@
+class Task:
+    def __init__(self, name):
+        self.name = name
+        self.is_completed = False
+
+    def complete(self):
+        self.is_completed = True
+
+
+class TaskModel:
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, task):
+        self.taks.append(task)
+
+    def get_tasks(self):
+        return self.tasks
